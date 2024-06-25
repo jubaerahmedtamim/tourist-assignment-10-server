51a203546068e6893b3e4d473b8b9db13e692bc8
jubaerahmedtamim <jubaerahmedtamim2014@gmail.com>
2024-06-26 05:48:13 +0600

    db user and pass key moved to env file

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.get('/', (req, res) => {
 // GreenExcursion 
 // 9LppdiqyTziXrJyW
 
-const uri = "mongodb+srv://GreenExcursion:9LppdiqyTziXrJyW@cluster0.yatfw0u.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yatfw0u.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
